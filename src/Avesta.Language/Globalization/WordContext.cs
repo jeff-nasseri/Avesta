27c4886cb45b0d22aeb759a3b59d9e34c38baa43
alireza nasseri <alirezanaseri383@yahoo.com>
2022-09-14 01:05:23 +0430

    update word context

--- src/Avesta.Language/Globalization/WordContext.cs
+++ src/Avesta.Language/Globalization/WordContext.cs
@@ -1,13 +1,34 @@
 ﻿using Avesta.Language.Globalization.Provider;
+using System.Data;
 using System.Threading.Tasks;
 
 namespace Avesta.Language.Globalization
 {
     public abstract class WordContext
     {
-        public async virtual Task OnCreate(LangContextProvider provider)
+        readonly LangContextProvider _provider;
+        public WordContext(LangContextProvider provider)
+        {
+            _provider = provider;
+        }
+
+        /// <summary>
+        /// Create a file or any context for writing language data on it
+        /// </summary>
+        public async virtual Task OnCreate()
         {
         }
+
+
+        /// <summary>
+        /// Fill all GlobalWord.Words type fields in application WordContext
+        /// </summary>
+        public async virtual Task OnInitialize()
+        {
+        }
+
+
+
     }
 
 }
