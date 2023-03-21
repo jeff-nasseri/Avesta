19ed1b895f9937943c77f9b577db46e6c25b081d
alireza nasseri <alirezanaseri383@yahoo.com>
2023-03-21 10:23:08 +0330

    init delete entity service - build fail

---
+++ src/Avesta.Services/Delete/IEntityDeleteService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using Avesta.Data.Model;
+using Avesta.Repository.EntityRepository.Delete;
+using Avesta.Share.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avesta.Services.Delete
+{
+    public interface IEntityDeleteService<TId, TEntity, TModel>
+        where TId : class
+        where TEntity : BaseEntity<TId>
+        where TModel : BaseModel<TId>
+    {
+        Task Delete(TModel model, bool exceptionRaiseIfNotExist = false);
+        Task Delete(TId id, bool exceptionRaiseIfNotExist = false);
+        Task Delete(Expression<Func<TEntity, bool>> single, bool exceptionRaiseIfNotExist = false);
+        Task DeleteRange(IEnumerable<TModel> models);
+        Task DeleteRange(Expression<Func<TEntity, bool>> where);
+        Task SoftDelete(TId id, bool exceptionRaiseIfNotExist = false);
+    }
+
+
+
+
+ 
+
+
+}
